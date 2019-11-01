9df0f72c4a0413f754b9e03381c78d1cb4396ee5
Paul Dreik <github@pauldreik.se>
2019-11-01 20:47:21 +0100

    auto format

--- build_like_ossfuzz.sh
+++ build_like_ossfuzz.sh
@@ -7,88 +7,88 @@ set -eu
 
 variant=plain-noavx
 if [ ! -d build-$variant ] ; then
-mkdir build-$variant
-cd build-$variant
+  mkdir build-$variant
+  cd build-$variant
 
-cmake .. \
--GNinja \
--DCMAKE_BUILD_TYPE=Debug \
--DSIMDJSON_BUILD_STATIC=On \
--DENABLE_FUZZING=On \
--DSIMDJSON_FUZZ_LINKMAIN=On \
--DSIMDJSON_DISABLE_AVX=On
+  cmake .. \
+	-GNinja \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DSIMDJSON_BUILD_STATIC=On \
+	-DENABLE_FUZZING=On \
+	-DSIMDJSON_FUZZ_LINKMAIN=On \
+	-DSIMDJSON_DISABLE_AVX=On
 
-ninja
+  ninja
 
-cd ..
+  cd ..
 fi
 
 variant=plain-normal
 if [ ! -d build-$variant ] ; then
-mkdir build-$variant
-cd build-$variant
+  mkdir build-$variant
+  cd build-$variant
 
-cmake .. \
--GNinja \
--DCMAKE_BUILD_TYPE=Debug \
--DSIMDJSON_BUILD_STATIC=On \
--DENABLE_FUZZING=On \
--DSIMDJSON_FUZZ_LINKMAIN=On \
+  cmake .. \
+	-GNinja \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DSIMDJSON_BUILD_STATIC=On \
+	-DENABLE_FUZZING=On \
+	-DSIMDJSON_FUZZ_LINKMAIN=On \
 
-ninja
+	ninja
 
-cd ..
+  cd ..
 fi
 
 variant=ossfuzz-noavx
 if [ ! -d build-$variant ] ; then
 
-export CC=clang
-export CXX="clang++"
-export CFLAGS="-fsanitize=fuzzer-no-link,address -fno-sanitize-recover=undefined -mno-avx2 -mno-avx "
-export CXXFLAGS="-fsanitize=fuzzer-no-link,address -fno-sanitize-recover=undefined -mno-avx2 -mno-avx"
-export LIB_FUZZING_ENGINE="-fsanitize=fuzzer"
+  export CC=clang
+  export CXX="clang++"
+  export CFLAGS="-fsanitize=fuzzer-no-link,address -fno-sanitize-recover=undefined -mno-avx2 -mno-avx "
+  export CXXFLAGS="-fsanitize=fuzzer-no-link,address -fno-sanitize-recover=undefined -mno-avx2 -mno-avx"
+  export LIB_FUZZING_ENGINE="-fsanitize=fuzzer"
 
-mkdir build-ossfuzz-noavx
-cd build-ossfuzz-noavx
+  mkdir build-ossfuzz-noavx
+  cd build-ossfuzz-noavx
 
-cmake .. \
--GNinja \
--DCMAKE_BUILD_TYPE=Debug \
--DSIMDJSON_BUILD_STATIC=On \
--DENABLE_FUZZING=On \
--DSIMDJSON_FUZZ_LINKMAIN=Off \
--DSIMDJSON_FUZZ_LDFLAGS=$LIB_FUZZING_ENGINE \
-      -DSIMDJSON_DISABLE_AVX=On
+  cmake .. \
+	-GNinja \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DSIMDJSON_BUILD_STATIC=On \
+	-DENABLE_FUZZING=On \
+	-DSIMDJSON_FUZZ_LINKMAIN=Off \
+	-DSIMDJSON_FUZZ_LDFLAGS=$LIB_FUZZING_ENGINE \
+	-DSIMDJSON_DISABLE_AVX=On
 
-ninja
+  ninja
 
-cd ..
+  cd ..
 fi
 
 variant=ossfuzz-msan
 if [ ! -d build-$variant ] ; then
 
-export CC=clang-7
-export CXX="clang++-7"
-export CFLAGS="-fsanitize=fuzzer-no-link,memory -mno-avx2 -mno-avx "
-export CXXFLAGS="-fsanitize=fuzzer-no-link,memory -mno-avx2 -mno-avx"
-export LIB_FUZZING_ENGINE="-fsanitize=fuzzer,memory"
+  export CC=clang-7
+  export CXX="clang++-7"
+  export CFLAGS="-fsanitize=fuzzer-no-link,memory -mno-avx2 -mno-avx "
+  export CXXFLAGS="-fsanitize=fuzzer-no-link,memory -mno-avx2 -mno-avx"
+  export LIB_FUZZING_ENGINE="-fsanitize=fuzzer,memory"
 
-mkdir build-$variant
-cd build-$variant
+  mkdir build-$variant
+  cd build-$variant
 
-cmake .. \
--GNinja \
--DCMAKE_BUILD_TYPE=Debug \
--DSIMDJSON_BUILD_STATIC=On \
--DENABLE_FUZZING=On \
--DSIMDJSON_FUZZ_LINKMAIN=Off \
--DSIMDJSON_FUZZ_LDFLAGS=$LIB_FUZZING_ENGINE \
-      -DSIMDJSON_DISABLE_AVX=On
+  cmake .. \
+	-GNinja \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DSIMDJSON_BUILD_STATIC=On \
+	-DENABLE_FUZZING=On \
+	-DSIMDJSON_FUZZ_LINKMAIN=Off \
+	-DSIMDJSON_FUZZ_LDFLAGS=$LIB_FUZZING_ENGINE \
+	-DSIMDJSON_DISABLE_AVX=On
 
-ninja
+  ninja
 
-cd ..
+  cd ..
 fi
 
