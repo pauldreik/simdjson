0d70a9e923659012022be7cfd71b855e6ddbecf9
Paul Dreik <github@pauldreik.se>
2019-11-13 22:25:49 +0100

    script for generating a fuzz coverage build locally

---
+++ fuzz/measure_coverage.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# make a coverage build
+
+bdir=build-coverage
+if [ ! -d $bdir ] ; then
+    mkdir -p $bdir
+    cd $bdir
+
+    export CC=gcc
+    export CXX="g++"
+    export CFLAGS="-fprofile-arcs -ftest-coverage"
+    export CXXFLAGS="-fprofile-arcs -ftest-coverage"
+    export LDFLAGS="-fprofile-arcs -ftest-coverage"
+
+    cmake .. \
+          -GNinja \
+          -DCMAKE_BUILD_TYPE=Debug \
+          -DSIMDJSON_BUILD_STATIC=On \
+          -DENABLE_FUZZING=On \
+	  -DSIMDJSON_FUZZ_LINKMAIN=On
+    ninja
+    cd ..
+fi
+
+# run the fuzzers as normal, then run
+#gcovr -r . --html --html-details --sort-uncovered -o out.html
+# and view the results
