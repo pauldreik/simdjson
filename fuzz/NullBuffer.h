4a3761297c46270f28085512f4f4d761daf282da
Paul Dreik <github@pauldreik.se>
2019-11-13 21:42:50 +0100

    refactor and add fuzzer

--- fuzz/NullBuffer.h
+++ fuzz/NullBuffer.h
@@ -1,8 +1,7 @@
-#include "simdjson/jsonparser.h"
-#include <cstddef>
-#include <cstdint>
+
+#pragma once
+
 #include <iostream>
-#include <string>
 
 // from https://stackoverflow.com/a/8244052
 class NulStreambuf : public std::streambuf {
@@ -20,15 +19,3 @@ public:
   NulOStream() : std::ostream(this) {}
   NulStreambuf *rdbuf() { return this; }
 };
-
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-
-  try {
-    auto pj = simdjson::build_parsed_json(Data, Size);
-    NulOStream os;
-    bool ignored=pj.print_json(os);
-  } catch (...) {
-  }
-  return 0;
-}
